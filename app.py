85562615251ed4159576a6deab59abb44556e775
KartikUmraniya <145537980+KartikUmraniya@users.noreply.github.com>
2025-06-13 19:58:05 +0530

    Update app.py
    removed an small error

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if model is not None:
                 col1, col2 = st.columns(2)
 
                 with col1:
-                    st.image(image, caption="Uploaded Image", use_column_width=True)
+                    st.image(image, caption="Uploaded Image", use_container_width=True)
 
                 img_array = process_image(image)
                 predictions = predict_anomaly(img_array)
